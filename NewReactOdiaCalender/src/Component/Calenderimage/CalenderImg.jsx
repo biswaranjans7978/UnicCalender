a8dca9764724f250cb5cff75bb0522e02840ad52
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-12-01 13:02:40 +0530

    upload original image

--- NewReactOdiaCalender/src/Component/Calenderimage/CalenderImg.jsx
+++ NewReactOdiaCalender/src/Component/Calenderimage/CalenderImg.jsx
@@ -1,6 +1,29 @@
 import React, { useState } from 'react'
 import CalenderCard from './CalenderCard'
-import author from "../../assets/photos/rasiphala.jpeg"
+import author1 from "../../assets/photos/january.jpeg"
+import author2 from "../../assets/photos/jan-rasi.jpeg"
+import author3 from "../../assets/photos/february.jpeg"
+import author4 from "../../assets/photos/feb-rasi.jpeg"
+import author5 from "../../assets/photos/march.jpeg"
+import author6 from "../../assets/photos/mar-rasi.jpeg"
+import author7 from "../../assets/photos/april.jpeg"
+import author8 from "../../assets/photos/apr-rasi.jpeg"
+import author9 from "../../assets/photos/may.jpeg"
+import author10 from "../../assets/photos/may-rasi.jpeg"
+import author11 from "../../assets/photos/june.jpeg"
+import author12 from "../../assets/photos/june-rasi.jpeg"
+import author13 from "../../assets/photos/july.jpeg"
+import author14 from "../../assets/photos/july-rasi.jpeg"
+import author15 from "../../assets/photos/august.jpeg"
+import author16 from "../../assets/photos/aug-rasi.jpeg"
+import author17 from "../../assets/photos/september.jpeg"
+import author18 from "../../assets/photos/sept-rasi.jpeg"
+import author19 from "../../assets/photos/october.jpeg"
+import author20 from "../../assets/photos/oct-rasi.jpeg"
+import author21 from "../../assets/photos/november.jpeg"
+import author22 from "../../assets/photos/nov-rasi.jpeg"
+import author23 from "../../assets/photos/december.jpeg"
+import author24 from "../../assets/photos/dec-rasi.jpeg"
 
 
 
@@ -8,101 +31,88 @@ function CalenderImg() {
 
   let frontCards=[
     {
-    
       monthName:"ଜାନୁଆରୀ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-January-Nua-Odisha.jpg",
+      cardImg:author1,
       rasiphala:"ରାଶିଫଳ",
       time:"10.10",
-      cardImg2:author,
-      
+      cardImg2:author2, 
     },
     {
       monthName:"ଫେବୃଆରୀ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-February-Nua-Odisha.jpg",
+      cardImg:author3,
       rasiphala:"ରାଶିଫଳ",
-      time:"12.10"
-      ,
-      cardImg2:author,
+      time:"12.10",
+      cardImg2:author4,
     },
     {
       monthName:"ମାର୍ଚ୍ଚ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-March-Nua-Odisha.jpg",
+      cardImg:author5,
       rasiphala:"ରାଶିଫଳ",
-      time:"16.10"
-      ,
-      cardImg2:author,
+      time:"16.10",
+      cardImg2:author6,
     },
     {
       monthName:"ଅପ୍ରେଲ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author7,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author8,
     },
     {
       monthName:"ମଇ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author9,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author10,
     },
     {
       monthName:"ଜୁନ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author11,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author12,
     },
     {
       monthName:"ଜୁଲାଇ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author13,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author14,
     },
     {
       monthName:"ଅଗଷ୍ଟ",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author15,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author16,
     },
     {
       monthName:"ସେପ୍ଟେମ୍ବର",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author17,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author18,
     },
     {
       monthName:"ଅକ୍ଟୋବର",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author19,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author20,
     },
     {
       monthName:"ନଭେମ୍ୱର",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author21,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author22,
     },
     {
       monthName:"ଡିସେମ୍ବର",
-      cardImg:"https://www.nuaodisha.com/images/odia-calendar/Odia-Kohinoor-Calendar-April-Nua-Odisha.jpg",
+      cardImg:author23,
       rasiphala:"ରାଶିଫଳ",
-      time:"20.10"
-      ,
-      cardImg2:author,
+      time:"20.10",
+      cardImg2:author24,
     }, 
   ]
     
